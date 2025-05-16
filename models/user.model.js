7220a90dc968b1e5cef96bd39541ec97717ad72b
rnickname <rnickname>
2025-05-16 17:20:55 +0330

    define model for database

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/Sequelize-connect')
+
+const User = sequelize.define(
+    'User',
+    {
+        // Model attributes are defined here
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {},);
+
+module.exports = User;
